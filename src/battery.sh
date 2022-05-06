79597fda9836d39d037b9673c2d1d22e13bd6567
Abhishek Dwaraki <abhishek.dwaraki@gmail.com>
2022-05-05 21:47:33 -0500

    Update for Big Sur/Monterey. Supports Apple Silicon.
    1. Updated battery details for Big Sur and later. 
    
    2. ManufactureDate is now calculated differently.
    
    3. Additionally, calculating health and percentage remaining is more precise using AppleRawMaxCapacity and AppleRawCurrentCapacity, than using the regular CurrentCapacity and MaxCapacity.

--- src/battery.sh
+++ src/battery.sh
@@ -1,117 +1,113 @@
-#!/usr/bin/env bash
+ioreg -l -n AppleSmartBattery -r > info.txt
+RAW_CURRENT_CAPACITY=$(cat info.txt | grep -e \"AppleRawCurrentCapacity\" | awk '{printf ("%i", $3)}')
+RAW_MAX_CAPACITY=$(cat info.txt | grep -e \"AppleRawMaxCapacity\" | awk '{printf ("%i", $3)}')
+# MAX_CAPACITY=$(cat info.txt | grep -e \"MaxCapacity\" | | awk -F',' '{printf ("%s", $45)}' | awk -F'=' '{printf      ("%i", $2)}')
+DESIGN_CAPACITY=$(cat info.txt | grep -e \"DesignCapacity\" | awk '{printf ("%i", $3)}')
+TEMPERATURE=$(cat info.txt | grep -e "\"Temperature\"" | awk '{printf ("%.1f", $3/100.00)}')
+MANUFACTURE_DATE=$(cat info.txt | grep -e \"ManufactureDate\" | awk -F',' '{printf ("%s", $56)}' | awk -F'=' '{printf ("%i", $2)}')
+CHARGING=$(cat info.txt | grep -e \"IsCharging\" | awk '{printf("%s",$3)}')
 
-INFO=$(ioreg -l -n AppleSmartBattery -r)
 
-# Charge and time remaining
-CURRENT_CAPACITY=$(echo "$INFO" | grep CurrentCapacity | awk '{printf $3; exit}')
-MAX_CAPACITY=$(echo "$INFO" | grep MaxCapacity | awk '{printf $3; exit}')
-CHARGE=$((CURRENT_CAPACITY * 100 / MAX_CAPACITY))
-CELLS=$(python -c "f='●'*($CHARGE/10) + '○'*(10-$CHARGE/10); print f")
-STATUS_INFO=Draining...
+SERIAL=$(cat info.txt | grep -e \"Serial\" | awk -F',' '{printf ("%s", $40)}' | awk -F'=' '{printf ("%s", $2)}' | tr -d '"')
+SERIAL="${SERIAL%\"}"
 
-CHARGING=$(echo "$INFO" | grep -i ischarging | awk '{printf("%s", $3)}')
-TIME_TO_EMPTY=$(echo "$INFO" | grep -i AvgTimeToEmpty | awk '{printf("%s", $3)}')
+HEALTH=$(echo $RAW_MAX_CAPACITY $DESIGN_CAPACITY | awk '{printf ("%i", $1/$2 * 100)}')
+CHARGE=$(echo $RAW_CURRENT_CAPACITY $RAW_MAX_CAPACITY | awk '{printf ("%i", $1/$2 * 100)}')
+CELLS=$(python3 -c "f='●'*($CHARGE/10) + '○'*(10-$CHARGE/10);print f")
+
+CYCLE_COUNT=$(cat info.txt | grep -e '\"CycleCount\" =' | awk '{printf ("%i", $3)}')
+
+TIME_TO_EMPTY=$(cat info.txt | grep -e \"AvgTimeToEmpty\" | awk '{printf("%s", $3)}')
 TIME_LEFT=Calculating…
-
-if [ "$TIME_TO_EMPTY" -lt 15000 ]; then
-    TIME_LEFT=$(echo "$INFO" | grep -i AvgTimeToEmpty | awk '{printf("%i:%.2i", $3/60, $3%60)}')
+if [ $TIME_TO_EMPTY -lt 15000 ]; then
+TIME_LEFT=$(cat info.txt | grep -e \"AvgTimeToEmpty\" | awk '{printf("%i:%.2i", $3/60, $3%60)}')
 fi
 
-if [ "$CHARGING" == Yes ]; then
-    TIME_FULL=$(echo "$INFO" | grep -i AvgTimeToFull | tr '\n' ' | ' | awk '{printf("%i:%.2i", $3/60, $3%60)}')
-    TIME_INFO=$(echo "$TIME_FULL" until full)
-    STATUS_INFO=Charging...
-    BATT_ICON=charging.png
+TIME_INFO=n
+STATUS_INFO=Draining
+BATT_ICON=icon.png
+
+if [ $CHARGING == Yes ]; then
+	TIME_FULL=$(cat info.txt | grep -i AvgTimeToFull | tr '\n' ' | ' | awk '{printf("%i:%.2i", $3/60, $3%60)}')
+	TIME_INFO=$(echo $TIME_FULL until full)
+	STATUS_INFO=Charging
+	BATT_ICON=charging.png
 else
-    FULLY_CHARGED=$(echo "$INFO" | grep -i FullyCharged | awk '{printf("%s", $3)}')
-    EXTERNAL=$(echo "$INFO" | grep -i ExternalConnected | awk '{printf("%s", $3)}')
-    if [ "$FULLY_CHARGED" == Yes ]; then
-        if [ "$EXTERNAL" == Yes ]; then
-            TIME_INFO="On AC power"
-            STATUS_INFO="Fully Charged"
-            BATT_ICON=power.png
-            CHARGE="100"
-            CELLS="●●●●●●●●●●"
-        else
-            TIME_INFO=$TIME_LEFT
-            BATT_ICON=full.png
-        fi
-    else
-        TIME_INFO=$TIME_LEFT
-        BATT_ICON=critical.png
-        if [ "$CHARGE" -gt 80 ]; then
-            BATT_ICON=full.png
-        elif [ "$CHARGE" -gt 50 ]; then
-            BATT_ICON=medium.png
-        elif [ "$CHARGE" -gt 10 ]; then
-            BATT_ICON=low.png
-        fi
-    fi
+	FULLY_CHARGED=$(cat info.txt | grep -e \"FullyCharged\" | awk '{printf("%s",$3)}')
+	EXTERNAL=$(cat info.txt | grep -e \"ExternalConnected\" | awk '{printf("%s",$3)}')
+	if [ $FULLY_CHARGED == Yes ]; then 
+		if [ $EXTERNAL == Yes ]; then
+			TIME_INFO=$(echo On AC power)
+			STATUS_INFO=$(echo Fully Charged)
+			BATT_ICON=power.png
+		else
+			TIME_INFO=$(echo $TIME_LEFT)
+			BATT_ICON=full.png
+		fi
+	else
+		TIME_INFO=$(echo $TIME_LEFT)
+		BATT_ICON=critical.png
+		if [ $CHARGE -gt 80 ]; then
+			BATT_ICON=full.png
+		elif [ $CHARGE -gt 50 ]; then
+			BATT_ICON=medium.png
+		elif [ $CHARGE -gt 10 ]; then
+			BATT_ICON=low.png
+		fi
+	fi
 fi
-
-# Temperature
-TEMPERATURE=$(echo "$INFO" | grep Temperature | awk '{printf ("%.1f", $3/10-273)}')
-
-# Cycle count
-CYCLE_COUNT=$(echo "$INFO" | grep -e '"CycleCount" =' | awk '{printf ("%i", $3)}')
-
-# Battery health
-# ref: https://github.com/BenziAhamed/alfred-battery/issues/10#issuecomment-745541202
-DESIGN_CAPACITY=$(echo "$INFO" | grep DesignCapacity | awk '{printf ("%.i", $3)}')
-HEALTH=$((MAX_CAPACITY * 100 / DESIGN_CAPACITY))
-
-if [ "$HEALTH" -gt 100 ]; then
-    HEALTH=100
-fi
-
-# Serial
-SERIAL=$(echo "$INFO" | grep BatterySerialNumber | awk '{printf ("%s", $3)}' | tr -d '"')
 
 # Battery age
-MANUFACTURE_DATE=$(echo "$INFO" | grep ManufactureDate | awk '{printf ("%i", $3)}')
-day=$((MANUFACTURE_DATE&31))
-month=$(((MANUFACTURE_DATE>>5)&15))
-year=$((1980+(MANUFACTURE_DATE>>9)))
-AGE=$(python -c "from datetime import date as D; d1=D.today(); d2=D($year, $month, $day); print ( (d1.year - d2.year)*12 + d1.month - d2.month )")
+# let "day=MANUFACTURE_DATE&31"
+# let "month=((MANUFACTURE_DATE>>5)&15)"
+# let "year=1980+(MANUFACTURE_DATE>>9)"
 
-# Alfred feedback
+let "MANUFACTURE_DATE=MANUFACTURE_DATE-0x0000303030303030"
+let "MANUFACTURE_DATE=(MANUFACTURE_DATE & 0x000000FF00FF00FF) * 10 + ((MANUFACTURE_DATE & 0x0000FF00FF00FF00) >> 8) + 1992"
+
+let "year=(MANUFACTURE_DATE >> 0) & 0xFFFF"
+let "month=(MANUFACTURE_DATE >> 16) & 0xFFFF"
+let "day=(MANUFACTURE_DATE >> 32) & 0xFFFF"
+
+AGE=$(python3 -c "from datetime import date as D; d1=D.today(); d2=D($year, $month, $day); print ( (d1.year - d2.year)*12 + d1.month - d2.month )")
+
 cat << EOB
 <?xml version="1.0"?>
 <items>
   <item>
     <title>$CHARGE% $CELLS</title>
 	<subtitle>$STATUS_INFO</subtitle>
-	<icon>icons/$BATT_ICON</icon>
+	<icon>$BATT_ICON</icon>
   </item>
   <item>
     <title>$TIME_INFO</title>
 	<subtitle>Time Left</subtitle>
-	<icon>icons/clock.png</icon>
+	<icon>clock.png</icon>
   </item>
   <item>
-    <title>$TEMPERATURE °C</title>
+    <title>${TEMPERATURE} °C</title>
 	<subtitle>Temperature</subtitle>
-	<icon>icons/temp.png</icon>
+	<icon>temp.png</icon>
   </item>
   <item>
     <title>$CYCLE_COUNT</title>
 	<subtitle>Charge Cycles Completed</subtitle>
-	<icon>icons/cycles.png</icon>
+	<icon>cycles.png</icon>
   </item>
   <item>
     <title>$HEALTH%</title>
 	<subtitle>Health</subtitle>
-	<icon>icons/health.png</icon>
+	<icon>health.png</icon>
   </item>
   <item>
     <title>$SERIAL</title>
 	<subtitle>Serial</subtitle>
-	<icon>icons/serial.png</icon>
+	<icon>serial.png</icon>
   </item>
   <item>
     <title>$AGE months</title>
 	<subtitle>Age</subtitle>
-	<icon>icons/age.png</icon>
+	<icon>age.png</icon>
   </item>
 </items>
 EOB
