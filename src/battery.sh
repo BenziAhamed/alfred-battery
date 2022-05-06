56e31a8cf33a8f1f93b15997b5031cbf71c1e896
Abhishek Dwaraki <abhishek.dwaraki@gmail.com>
2022-05-06 03:03:45 -0500

    Mouse/Trackpad Support and Code Cleanup.
    1. Integrated zenopopovici's code in the issue tracker comments to provide mouse, trackpad and keyboard support.
    
    2. Changed CELLS code to be compatible with Python3 (still works with Py2).

--- src/battery.sh
+++ src/battery.sh
@@ -13,7 +13,7 @@ SERIAL="${SERIAL%\"}"
 
 HEALTH=$(echo $RAW_MAX_CAPACITY $DESIGN_CAPACITY | awk '{printf ("%i", $1/$2 * 100)}')
 CHARGE=$(echo $RAW_CURRENT_CAPACITY $RAW_MAX_CAPACITY | awk '{printf ("%i", $1/$2 * 100)}')
-CELLS=$(python3 -c "f='●'*($CHARGE/10) + '○'*(10-$CHARGE/10);print f")
+CELLS=$(/usr/bin/python3 -c "f='●'*(int($CHARGE/10)) + '○'*(int(10-$CHARGE/10));print(f)")
 
 CYCLE_COUNT=$(cat info.txt | grep -e '\"CycleCount\" =' | awk '{printf ("%i", $3)}')
 
@@ -25,13 +25,13 @@ fi
 
 TIME_INFO=n
 STATUS_INFO=Draining
-BATT_ICON=icon.png
+BATT_ICON="gradient-icons/draining.png"
 
 if [ $CHARGING == Yes ]; then
-	TIME_FULL=$(cat info.txt | grep -i AvgTimeToFull | tr '\n' ' | ' | awk '{printf("%i:%.2i", $3/60, $3%60)}')
+	TIME_FULL=$(cat info.txt | grep -e \"AvgTimeToFull\" | tr '\n' ' | ' | awk '{printf("%i:%.2i", $3/60, $3%60)}')
 	TIME_INFO=$(echo $TIME_FULL until full)
 	STATUS_INFO=Charging
-	BATT_ICON=charging.png
+	BATT_ICON="gradient-icons/charging.png"
 else
 	FULLY_CHARGED=$(cat info.txt | grep -e \"FullyCharged\" | awk '{printf("%s",$3)}')
 	EXTERNAL=$(cat info.txt | grep -e \"ExternalConnected\" | awk '{printf("%s",$3)}')
@@ -39,28 +39,23 @@ else
 		if [ $EXTERNAL == Yes ]; then
 			TIME_INFO=$(echo On AC power)
 			STATUS_INFO=$(echo Fully Charged)
-			BATT_ICON=power.png
+			BATT_ICON="gradient-icons/power.png"
 		else
 			TIME_INFO=$(echo $TIME_LEFT)
-			BATT_ICON=full.png
+			BATT_ICON="gradient-icons/full.png"
 		fi
 	else
 		TIME_INFO=$(echo $TIME_LEFT)
-		BATT_ICON=critical.png
+		BATT_ICON="gradient-icons/critical.png"
 		if [ $CHARGE -gt 80 ]; then
-			BATT_ICON=full.png
+			BATT_ICON="gradient-icons/full.png"
 		elif [ $CHARGE -gt 50 ]; then
-			BATT_ICON=medium.png
+			BATT_ICON="gradient-icons/medium.png"
 		elif [ $CHARGE -gt 10 ]; then
-			BATT_ICON=low.png
+			BATT_ICON="gradient-icons/low.png"
 		fi
 	fi
 fi
-
-# Battery age
-# let "day=MANUFACTURE_DATE&31"
-# let "month=((MANUFACTURE_DATE>>5)&15)"
-# let "year=1980+(MANUFACTURE_DATE>>9)"
 
 let "MANUFACTURE_DATE=MANUFACTURE_DATE-0x0000303030303030"
 let "MANUFACTURE_DATE=(MANUFACTURE_DATE & 0x000000FF00FF00FF) * 10 + ((MANUFACTURE_DATE & 0x0000FF00FF00FF00) >> 8) + 1992"
@@ -71,43 +66,91 @@ let "day=(MANUFACTURE_DATE >> 32) & 0xFFFF"
 
 AGE=$(python3 -c "from datetime import date as D; d1=D.today(); d2=D($year, $month, $day); print ( (d1.year - d2.year)*12 + d1.month - d2.month )")
 
+TRACKPAD_ICON="gradient-icons/trackpad.png"
+# trackpad
+TrackpadPercent=`ioreg -c AppleDeviceManagementHIDEventService | grep 'Magic Trackpad' -A8 | grep BatteryPercent | sed 's/[a-z,A-Z, ,|,\",=]//g' | tail -1 | awk '{print $1}'`
+if [ ${#TrackpadPercent} = 0 ]
+then
+	TrackpadTitle="Not connected"
+else
+	TrackpadSlug=$(python -c "f='●'*($TrackpadPercent/10) + '○'*(10-$TrackpadPercent/10);print f")
+	TrackpadTitle="$TrackpadPercent% $TrackpadSlug"
+fi
+
+MOUSE_ICON="gradient-icons/mouse.png"
+# mouse
+MousePercent=`ioreg -c AppleDeviceManagementHIDEventService | grep \"Magic Mouse\" -A8 | grep BatteryPercent | sed 's/[a-z,A-Z, ,|,\",=]//g' | tail -1 | awk '{print $1}'`
+if [ ${#MousePercent} = 0 ]
+then
+	MouseTitle="Not connected"
+else
+	MouseSlug=$(python -c "f='●'*($MousePercent/10) + '○'*(10-$MousePercent/10);print f")
+	MouseTitle="$MousePercent% $MouseSlug"
+fi
+
+KEYBOARD_ICON="gradient-icons/keyboard.png"
+# keyboard
+KeyboardPercent=`ioreg -c AppleDeviceManagementHIDEventService | grep -e \"Magic Keyboard\" -A8 | grep BatteryPercent | sed 's/[a-z,A-Z, ,|,\",=]//g' | tail -1 | awk '{print $1}'`
+if [ ${#KeyboardPercent} = 0 ]
+then
+	KeyboardTitle="Not connected"
+else
+	KeyboardSlug=$(python -c "f='●'*($KeyboardPercent/10) + '○'*(10-$KeyboardPercent/10);print f")
+	KeyboardTitle="$KeyboardPercent% $KeyboardSlug"
+fi
+
 cat << EOB
 <?xml version="1.0"?>
 <items>
   <item>
     <title>$CHARGE% $CELLS</title>
-	<subtitle>$STATUS_INFO</subtitle>
-	<icon>$BATT_ICON</icon>
+	  <subtitle>$STATUS_INFO</subtitle>
+	  <icon>$BATT_ICON</icon>
   </item>
   <item>
     <title>$TIME_INFO</title>
-	<subtitle>Time Left</subtitle>
-	<icon>clock.png</icon>
+	  <subtitle>Time Left</subtitle>
+	  <icon>gradient-icons/clock.png</icon>
   </item>
   <item>
     <title>${TEMPERATURE} °C</title>
-	<subtitle>Temperature</subtitle>
-	<icon>temp.png</icon>
+	  <subtitle>Temperature</subtitle>
+	  <icon>gradient-icons/temp.png</icon>
   </item>
   <item>
     <title>$CYCLE_COUNT</title>
-	<subtitle>Charge Cycles Completed</subtitle>
-	<icon>cycles.png</icon>
+	  <subtitle>Charge Cycles Completed</subtitle>
+	  <icon>gradient-icons/cycles.png</icon>
   </item>
   <item>
     <title>$HEALTH%</title>
-	<subtitle>Health</subtitle>
-	<icon>health.png</icon>
+	  <subtitle>Health</subtitle>
+	  <icon>gradient-icons/health.png</icon>
+  </item>
+  <item>
+    <title>$MouseTitle</title>
+    <subtitle>Magic Mouse</subtitle>
+    <icon>$MOUSE_ICON</icon>
+  </item>
+  <item>
+    <title>$KeyboardTitle</title>
+    <subtitle>Magic Keyboard</subtitle>
+    <icon>$KEYBOARD_ICON</icon>
+  </item>
+  <item>
+    <title>$TrackpadTitle</title>
+    <subtitle>Magic TrackPad</subtitle>
+    <icon>$TRACKPAD_ICON</icon>
   </item>
   <item>
     <title>$SERIAL</title>
-	<subtitle>Serial</subtitle>
-	<icon>serial.png</icon>
+	  <subtitle>Serial</subtitle>
+	  <icon>gradient-icons/serial.png</icon>
   </item>
   <item>
     <title>$AGE months</title>
-	<subtitle>Age</subtitle>
-	<icon>age.png</icon>
+	  <subtitle>Age</subtitle>
+	  <icon>icons/age.png</icon>
   </item>
 </items>
 EOB
